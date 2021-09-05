1dab8b5705fb2065cc8330a0818211549ea999c2
Daniel Felipe Morales Pinzon <danielf.moralesp20@gmail.com>
2021-09-05 11:27:23 -0500

    feature: CRUD tabla tbl_escuela

--- source/src/App.java
+++ source/src/App.java
@@ -1,9 +1,7 @@
-import access.EscuelaDAO;
 import view.MasterWindows2;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println(new EscuelaDAO().findAll());
 		new MasterWindows2().setVisible(true);
 	}
 }
--- source/src/access/EscuelaDAO.java
+++ source/src/access/EscuelaDAO.java
@@ -45,10 +45,9 @@ public class EscuelaDAO {
     	
         return result;
     }
-    
-    
+     
     public List<EscuelaModel> findAll() {
-    	List<EscuelaModel> result = new ArrayList<EscuelaModel>();
+    	List<EscuelaModel> result = null;
     	
     	if(Optional.ofNullable(conn).isEmpty())
     		conn = ConnectionDB.getInstance().connect();
@@ -64,9 +63,12 @@ public class EscuelaDAO {
                 ResultSet resultQuery = statement.executeQuery();
                 
                 while (resultQuery.next()) {
+                	if(Optional.ofNullable(result).isEmpty())
+                		result = new ArrayList<EscuelaModel>();
+                	
                     EscuelaModel temp = new EscuelaModel(resultQuery.getInt(1), resultQuery.getString(2), 
                     		resultQuery.getString(3), resultQuery.getInt(4), resultQuery.getString(5), 
-                    		resultQuery.getString(5));
+                    		resultQuery.getString(6));
                     
                     result.add(temp);
                 }
@@ -74,6 +76,123 @@ public class EscuelaDAO {
 				e.printStackTrace();
 			}
     	}
+    	
+    	return result;
+    }
+    
+    public List<EscuelaModel> findAllLikeId(int id) {
+    	List<EscuelaModel> result = null;
+    	
+    	if(Optional.ofNullable(conn).isEmpty())
+    		conn = ConnectionDB.getInstance().connect();
+    		
+    	if(Optional.ofNullable(conn).isPresent()) {
+    		try {
+    			String sql = "SELECT "
+        				+ "id, nombre, habilidad, anios_servicio, creador, fecha_inicio "
+        				+ "FROM "
+        				+ "tbl_escuela "
+        				+ "WHERE id LIKE ?";
+    			
+    			PreparedStatement statement = conn.prepareStatement(sql);
+    			statement.setString(1, "%"+ id +"%");
+    			
+                ResultSet resultQuery = statement.executeQuery();
+                while (resultQuery.next()) {
+                	if(Optional.ofNullable(result).isEmpty())
+                		result = new ArrayList<EscuelaModel>();
+                	
+                    EscuelaModel temp = new EscuelaModel(resultQuery.getInt(1), resultQuery.getString(2), 
+                    		resultQuery.getString(3), resultQuery.getInt(4), resultQuery.getString(5), 
+                    		resultQuery.getString(6));
+                    
+                    result.add(temp);
+                }
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+    	}
+    	
+    	return result;
+    }
+    
+    public boolean save(EscuelaModel newEscuela) {
+    	boolean result = false;
+    	
+    	if(Optional.ofNullable(conn).isEmpty())
+    		conn = ConnectionDB.getInstance().connect();
+    		
+    	if(Optional.ofNullable(conn).isPresent()) {
+    		try {
+    			 String sql = "INSERT INTO "
+    			 		+ "tbl_escuela(id, nombre, habilidad, anios_servicio, creador, fecha_inicio) "
+    			 		+ "VALUES (?, ?, ?, ?, ?, ?);";
+    			 
+    			PreparedStatement statement = conn.prepareStatement(sql);
+     			statement.setInt(1, newEscuela.getCodigo_escuela());
+     			statement.setString(2, newEscuela.getNombre_escuela());
+     			statement.setString(3, newEscuela.getHabilidad());
+     			statement.setInt(4, newEscuela.getAnios_servicio());
+     			statement.setString(5, newEscuela.getCreador());
+     			statement.setString(6, newEscuela.getFecha_inicio());
+     			
+     			result = statement.executeUpdate() > 0;
+    		} catch (Exception e) {
+    			e.printStackTrace();
+    		}
+		}
+    	
+    	return result;
+    }
+    
+    public boolean updateById(EscuelaModel newEscuela) {
+    	boolean result = false;
+    	
+    	if(Optional.ofNullable(conn).isEmpty())
+    		conn = ConnectionDB.getInstance().connect();
+    		
+    	if(Optional.ofNullable(conn).isPresent()) {
+    		try {
+    			 String sql = "UPDATE tbl_escuela "
+    			 		+ "SET nombre=?, habilidad=?, anios_servicio=?, creador=?, fecha_inicio=? "
+    			 		+ "WHERE id=?";
+    			 
+    			PreparedStatement statement = conn.prepareStatement(sql);
+     			statement.setString(1, newEscuela.getNombre_escuela());
+     			statement.setString(2, newEscuela.getHabilidad());
+     			statement.setInt(3, newEscuela.getAnios_servicio());
+     			statement.setString(4, newEscuela.getCreador());
+     			statement.setString(5, newEscuela.getFecha_inicio());
+     			statement.setInt(6, newEscuela.getCodigo_escuela());
+     			
+     			result = statement.executeUpdate() > 0;
+    		} catch (Exception e) {
+    			e.printStackTrace();
+    		}
+		}
+    	
+    	return result;
+    }
+    
+    public boolean deleteById(int id) {
+    	boolean result = false;
+    	
+    	if(Optional.ofNullable(conn).isEmpty())
+    		conn = ConnectionDB.getInstance().connect();
+    		
+    	if(Optional.ofNullable(conn).isPresent()) {
+    		try {
+    			 String sql = "DELETE FROM tbl_escuela "
+    			 		+ "WHERE id=?";
+    			 
+    			PreparedStatement statement = conn.prepareStatement(sql);
+     			statement.setInt(1, id);
+     			
+     			result = statement.executeUpdate() > 0;
+    		} catch (Exception e) {
+    			e.printStackTrace();
+    		}
+		}
     	
     	return result;
     }
