828e956cdac3fa10d69f97b7aa7822851dc79dd6
David McEwen <dmcewena@hotmail.com>
2021-09-08 15:29:29 -0500

    Mejoras sobre la consulta de escuela

--- source/src/controller/ConsultarPanelController.java
+++ source/src/controller/ConsultarPanelController.java
@@ -23,15 +23,15 @@ public class ConsultarPanelController {
 	}
 	
 	public void actionEventBtnConsultar(ActionEvent evt) {
+		DefaultTableModel model = (DefaultTableModel)view.getTable().getModel();
 		String codigo_consultar = view.getTxtFieldCod().getText();
-		
-        if (codigo_consultar.isBlank() || FieldUtils.isEmpty(codigo_consultar)) {
-            JOptionPane.showMessageDialog(null,"No se permite campo vacio","ALERTA",JOptionPane.WARNING_MESSAGE);
+        if(FieldUtils.isEmpty(codigo_consultar) || codigo_consultar.isBlank()) {
+        	model.setRowCount(0);
         } else if(!FieldUtils.isNumber(codigo_consultar)) {
-        	JOptionPane.showMessageDialog(null,"Tipo de dato erroneo","ALERTA",JOptionPane.WARNING_MESSAGE); 
+        	JOptionPane.showMessageDialog(null,"Tipo de dato erroneo","ALERTA",JOptionPane.WARNING_MESSAGE);
         } else {
         	List<EscuelaModel> result = repo.findAllLikeId(Integer.parseInt(codigo_consultar));
-        	DefaultTableModel model = (DefaultTableModel)view.getTable().getModel();
+        	
         	if (result!=null) {
         		model.setRowCount(0);
         		for(EscuelaModel registro : result) {
